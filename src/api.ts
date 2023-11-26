421d8b3692561b80b17a9ec09a0592635f448c1c
yoshiko <ttkmysk021@gmail.com>
2023-11-26 21:22:55 +0900

    secondsss

--- src/api.ts
+++ src/api.ts
@@ -1,48 +1,53 @@
+// api.ts
+
 import { Task } from "./types";
 
+// APIのベースURL
+const API_URL = 'https://your-api.vercel.app';
+
 export const getAllTodos = async (): Promise<Task[]> => {
-  const res = await fetch(`http://localhost:3001/tasks`, {
+  const res = await fetch(`${API_URL}/tasks`, {
     cache: "no-store", // SSR
   });
-  const todos = await res.json(); // awaitを追加
+  const todos = await res.json();
 
   return todos;
 };
 
 export const addTodo = async (todo: Task): Promise<Task> => {
-  const res = await fetch(`http://localhost:3001/tasks`, {
+  const res = await fetch(`${API_URL}/tasks`, {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
     body: JSON.stringify(todo),
   });
-  const newTodo = await res.json(); // awaitを追加
+  const newTodo = await res.json();
 
   return newTodo;
 };
 
 export const editTodo = async (id: string, newText: string): Promise<Task> => {
-  const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+  const res = await fetch(`${API_URL}/tasks/${id}`, {
     method: "PUT",
     headers: {
       "Content-type": "application/json",
     },
     body: JSON.stringify({ text: newText }),
   });
-  const updatedTodo = await res.json(); // awaitを追加
+  const updatedTodo = await res.json();
 
   return updatedTodo;
 };
 
 export const deleteTodo = async (id: string): Promise<Task> => {
-  const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+  const res = await fetch(`${API_URL}/tasks/${id}`, {
     method: "DELETE",
     headers: {
       "Content-type": "application/json",
     },
   });
-  const deletedTodo = await res.json(); // awaitを追加
+  const deletedTodo = await res.json();
 
   return deletedTodo;
 };
