8f847a1e117561951be650520a3d17eacb598dd7
yoshiko <ttkmysk021@gmail.com>
2023-11-29 15:15:17 +0900

    fouthss

--- next.config.js
+++ next.config.js
@@ -3,15 +3,9 @@ const dotenv = require('dotenv');
 // Load environment variables from .env file
 dotenv.config();
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-  // Rest of your next.config.js configuration
-// };
-
-module.exports = {
-  // No need to set basePath during development
-  // basePath: process.env.BASE_PATH || '',
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Rest of your next.config.js configuration
 };
 
+module.exports = nextConfig;
